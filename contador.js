482002e0451059823832e08856a8cd85116176b5
NicolasSuzigan <nicolas_suzigan@hotmail.com>
2021-07-09 18:41:24 -0300

    first commit

---
+++ contador.js
@@ -0,0 +1,35 @@
+window.addEventListener("load", () => {
+
+//tempo em segundos.
+let sec = 180;
+
+const countDiv = document.getElementById("timer");
+
+const secpass = () => {
+
+  let min = Math.floor(sec/60);
+  let segundosRestantes = sec % 60;
+
+  if(segundosRestantes < 10){
+    min = "0" + segundosRestantes;
+  }
+
+  if(min < 10){
+    min = "0" + min;
+  }
+
+  //vai gerar o formato 00:02:59
+  countDiv.innerHTML = "00:" + min + ":" + segundosRestantes;
+
+  //condição final
+    if(sec > 0){
+      sec = sec-1;
+    }
+    else{
+      countDiv.innerHTML = "Acabou!"
+    }
+}
+
+const countDown = setInterval(() => secpass(), 1000);
+
+});
